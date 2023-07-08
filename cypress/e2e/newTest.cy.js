98031225e9ad9ca973b5530c754a9965b064d120
sagar6502 <ssunar@bayonesolutions.com>
2023-07-08 14:24:49 +0530

    sagar update

--- cypress/e2e/newTest.cy.js
+++ cypress/e2e/newTest.cy.js
@@ -1,7 +1,8 @@
 describe('My ganesh Test', () => {
     it('test-negative', () => {
         cy.visit("https://www.forhims.com/")
-
+        //sagar vai
+        cy.visit("Sagar Sunar")
         cy.title().should('eq', 'forhims')
     })
   })
