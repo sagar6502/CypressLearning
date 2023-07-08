d9b85ba33fc980596c1f688ba7a5a7b9b123cf2d
ganesh kr sah <ganeshkumar201999@gmail.com>
2023-07-08 14:22:31 +0530

    Second update

--- cypress/e2e/newTest.cy.js
+++ cypress/e2e/newTest.cy.js
@@ -1,7 +1,8 @@
 describe('My ganesh Test', () => {
     it('test-negative', () => {
         cy.visit("https://www.forhims.com/")
-
+        //ganya vai
+        cy.visit("Ganesh Sah")
         cy.title().should('eq', 'forhims')
     })
   })
